d70926b82ecdb85955737925cdbc094e492aea87
Snake <snake.fly@hotmail.com>
2013-12-11 14:58:27 +0800

    add crontab to compute tag stat

--- webroot/Lib/Action/TagStatAction.class.php
+++ webroot/Lib/Action/TagStatAction.class.php
@@ -11,7 +11,14 @@ class TagStatAction extends Action {
     function index() {
         $model = TagStatModel::getModel();
         $sql = 'select * from ' . TagStatModel::tableName . ';';
-        $data = array_values($model->query($sql));
+        $rows = array_values($model->query($sql));
+        $data = array();
+        foreach ( $rows as $row ) {
+            $tag = $row [TagStatModel::tag_name];
+            $user_num = $row [TagStatModel::user_num];
+            $click_num = $row [TagStatModel::click_num];
+            array_push($data, array($tag, $user_num, $click_num));
+        }
 
         $this->assign ( 'data', $data );
         $this->display();
