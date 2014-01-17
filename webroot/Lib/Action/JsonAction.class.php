40dd7529d77566fbc39689c1f1e5f18b9370b555
witwolf <wo.bian.shi.mo0@gmail.com>
2014-01-17 11:15:35 +0800

    .

---
+++ webroot/Lib/Action/JsonAction.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: witwolf
+ * Date: 1/16/14
+ * Time: 5:35 PM
+ */
+class JsonAction extends CountryAction {
+    public $name = 'JsonAction';
+
+    public $model_name = 'JsonModel';
+}
---
+++ webroot/Lib/Model/JsonModel.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: witwolf
+ * Date: 1/16/14
+ * Time: 5:37 PM
+ */
+
+class JsonModel extends Model{
+
+}
